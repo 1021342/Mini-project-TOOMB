c5ceb9e3d27a527e435ae13ce6536aead3a5eac3
1021342 <1021342@hr.nl>
2024-02-15 16:10:51 +0100

    program + player

---
+++ Player.cs
@@ -0,0 +1,17 @@
+﻿class Player
+{
+    public int CurrentHitPoints;
+    public string CurrentLocation; //location object of string?
+    public string CurrentWeapon; //weapon object of string?
+    public int MaximumHitPoints;
+    public string Name;
+
+    public Player(int CurrentHitPoints, string CurrentLocation, string CurrentWeapon, int MaximumHitPoints, string Name)
+    {
+        this.CurrentHitPoints = CurrentHitPoints;
+        this.CurrentLocation = CurrentLocation;
+        this.CurrentWeapon = CurrentWeapon;
+        this.MaximumHitPoints = MaximumHitPoints;
+        this.Name = Name;
+    }
+}
