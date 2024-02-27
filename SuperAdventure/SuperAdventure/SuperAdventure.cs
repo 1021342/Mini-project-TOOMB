f0be5b4382b460fa7cb42d3d13ccd210a6cbbcf7
Oli4vV <1079722@hr.nl>
2024-02-27 14:02:50 +0100

    Create SuperAdventure.cs

---
+++ SuperAdventure/SuperAdventure/SuperAdventure.cs
@@ -0,0 +1,11 @@
+﻿class SuperAdventure
+{
+    public string CurrentMonster;
+    public string ThePlayer;
+
+    public SuperAdventure(string CurrentMonster, string ThePlayer)
+    {
+        this.CurrentMonster = CurrentMonster;
+        this.ThePlayer = ThePlayer;
+    }
+}
